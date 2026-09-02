fbdfb12d4007b824765622115d0459b6bab0f5b8
AtlasOpsTech <ikram@atlasops.tech>
2026-09-01 17:53:42 -0700

    Add local/rolepathways/lang/en/local_rolepathways.php

---
+++ local/rolepathways/lang/en/local_rolepathways.php
@@ -0,0 +1,15 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Role pathways';
+$string['dashboard'] = 'My learning pathway';
+$string['rolepathways:view'] = 'View the Role Pathways dashboard';
+$string['privacy:metadata'] = 'The Role Pathways plugin does not store personal data.';
+
